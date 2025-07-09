d4273394b74c240e8f5fbef535046b3c44a1bb2f
tech-time-netizen <e2476168@gmail.com>
2025-07-09 17:36:02 +0300

    Create a user profile using props

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ export default function UserProfile(props) {
     <div>
           <h2>Name: {props.name}</h2>
           <p>Age: {props.age}</p>
-          <p>Bio: { props.bio}</p>
+          <p>Bio: {props.bio}</p>
     </div>
   )
 }
