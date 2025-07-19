b44040f5f5c5bc8659f22d5843373ff72df96ead
tech-time-netizen <e2476168@gmail.com>
2025-07-19 21:02:19 +0300

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from './UserContext'
 
-export default function UserProfile(props) {
+export default function UserProfile() {
+  const userData = useContext(UserContext)
   return (
     <div>
-          <h2>Name: {props.name}</h2>
-          <p>Age: {props.age}</p>
-          <p>Bio: {props.bio}</p>
+          <h2>Name: {userData.name}</h2>
+          <p>Age: {userData.age}</p>
+          <p>Bio: {userData.bio}</p>
     </div>
   )
 }
