757074c508e1e42e43968bd06e8e7d75db44574c
tech-time-netizen <e2476168@gmail.com>
2025-07-19 19:56:41 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MainContent() {
+  return (
+    <div>
+          <main style={{border:'10px solid black'}}>
+              <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </div>
+  )
+}
